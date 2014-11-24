48aca385e67cbc9fdbde97771f1a9f43233209bf
Ryan Foo <ryanfoo@nyu.edu>
2014-11-24 13:10:37 -0500

    Added header file implementation

--- nebuloMod/Source/LFOWaveformGenerator.cpp
+++ nebuloMod/Source/LFOWaveformGenerator.cpp
@@ -6,4 +6,11 @@
 //
 //
 
+/* 
+ * SAMPLING of the LFO Waveform Table
+ * 1) Takes samples from the table
+ * 2) Write samples into our buffer
+ * 3) Buffer is sent into LFO of Flanger/Phaser aspects of code
+ */
+
 #include "LFOWaveformGenerator.h"
--- nebuloMod/Source/LFOWaveformTable.cpp
+++ nebuloMod/Source/LFOWaveformTable.cpp
@@ -6,4 +6,11 @@
 //
 //
 
+/*
+ * GUI ASPECT of the LFO Waveform
+ * 1) Allows the user to draw a customizable waveform 
+ * 2) Only 2 movable positions and 1 steady position
+ * 3) Every time you move a position, store the sample into a buffer
+ */
+
 #include "LFOWaveformTable.h"
