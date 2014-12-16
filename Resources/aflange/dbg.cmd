9378f521733219548579ebc8ee8245b75d912222
Tom Longabaugh <tom.longabaugh@nyu.edu>
2014-12-15 19:37:45 -0500

    deleting resources and sdks

--- Resources/aflange/dbg.cmd
+++
@@ -1,6 +0,0 @@
-copy debug\* C:\tmp\NiallsPedalBoard\NiallsPedalBoard
-rem copy debug\aflange.* "c:\program files\steinberg\vstplugins\"
-copy debug\*.dll \users\dcronin\Documents\VSTPlugins
-\tmp\NiallsPedalBoard\NiallsPedalBoard\NiallsPedalBoard.exe
-
-
